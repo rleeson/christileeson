ac8159c2d246f30ab82c51784ab0e926f5acbb1f
Ryan Leeson <sysadmin@ryanleeson.com>
2023-03-14 02:20:58 -0400

    - Implement basic styling structure - Add logos and basic header styles

--- wp-content/themes/custom/christileeson/header.php
+++ wp-content/themes/custom/christileeson/header.php
@@ -24,66 +24,68 @@
 		<div class="header__logo">
 			<a class="header__logo-link" href="<?php echo esc_url( home_url() ); ?>" rel="home">
 				<span class="screen-reader-text"><?php bloginfo( 'name' ); ?><?php 
-					echo esc_html__( 'Home Page', 'christileeson' ); ?></span></a>
+					echo esc_html__( 'Christi Leeson Home Page', 'christileeson' ); ?></span></a>
 		</div>
-		<nav
-			class="header__main-navigation"
-			aria-label="<?php echo esc_attr__( 'Main navigation', 'christileeson' ); ?>"
-		>
-			<?php
-			wp_nav_menu(
-				[
-					'theme_location' => 'main-navigation',
-					'menu_id'        => 'main-navigation',
-					'container'      => false,
-					'echo'           => true,
-					'depth'          => 1
-				]
-			);
-			?>
-		</nav>
-		<button class="search__open" aria-expanded="false" aria-controls="header__search">
-			<span class="screen-reader-text"><?php echo esc_html__( 'Open Search', 'christileeson' ); ?></span></button>
-		<section
-			class="header__search"
-			aria-label="<?php echo esc_attr__( 'Search', 'christileeson' ); ?>"
-			aria-hidden="true"
-		><?php
-			get_search_form( [
-				'container_classes' => [ 'search__form' ],
-				'input_label_id'    => 'utility-menu-search',
-				'max_length'        => '200',
-				'placeholder_text'  => __( 'Search', 'christileeson' ),
-				'label_text'        => __( 'Search in ', 'christileeson' ) . home_url( '/' ),
-				'label_is_sr'       => true,
-			] );
-			?>
-			<button class="search__close">
-				<span class="screen-reader-text"><?php echo esc_html__( 'Close Search', 'christileeson' ); ?></span></button>
-		</section>
-		<div id="mobile__navigation">
-			<button class="mobile__menu-open" aria-controls="mobile-menu" aria-expanded="false">
-				<span class="screen-reader-text"><?php echo esc_html__( 'Open Menu', 'christileeson' ); ?></span></button>
-			<div id="mobile-menu" class="mobile__menu" aria-hidden="true">
-				<button class="mobile__menu-close" tabindex="-1"><span
-						tabindex="-1"
-						class="screen-reader-text"
-					><?php echo esc_html__( 'Close Menu', 'christileeson' ); ?></span></button>					
-				<nav
-					class="=mobile__main-navigation"
-					aria-label="<?php echo esc_attr__( 'Mobile main navigation', 'christileeson' ); ?>"
-				><?php
-					wp_nav_menu(
-						[
-							'theme_location' => 'main-navigation',
-							'menu_id'        => 'main-navigation',
-							'container'      => false,
-							'echo'           => true,
-							'depth'          => 1
-						]
-					);
-					?>
-				</nav>
+		<div class="header__controls">
+			<button class="search__open" aria-expanded="false" aria-controls="header__search">
+				<span class="screen-reader-text"><?php echo esc_html__( 'Open Search', 'christileeson' ); ?></span></button>
+			<section
+				class="header__search"
+				aria-label="<?php echo esc_attr__( 'Search', 'christileeson' ); ?>"
+				aria-hidden="true"
+			><?php
+				get_search_form( [
+					'container_classes' => [ 'search__form' ],
+					'input_label_id'    => 'utility-menu-search',
+					'max_length'        => '200',
+					'placeholder_text'  => __( 'Search', 'christileeson' ),
+					'label_text'        => __( 'Search in ', 'christileeson' ) . home_url( '/' ),
+					'label_is_sr'       => true,
+				] );
+				?>
+				<button class="search__close">
+					<span class="screen-reader-text"><?php echo esc_html__( 'Close Search', 'christileeson' ); ?></span></button>
+			</section>
+			<nav
+				class="header__main-navigation"
+				aria-label="<?php echo esc_attr__( 'Main navigation', 'christileeson' ); ?>"
+			>
+				<?php
+				wp_nav_menu(
+					[
+						'theme_location' => 'main-navigation',
+						'menu_id'        => 'main-navigation',
+						'container'      => false,
+						'echo'           => true,
+						'depth'          => 1
+					]
+				);
+				?>
+			</nav>
+			<div id="mobile__navigation">
+				<button class="mobile__menu-open" aria-controls="mobile-menu" aria-expanded="false">
+					<span class="screen-reader-text"><?php echo esc_html__( 'Open Menu', 'christileeson' ); ?></span></button>
+				<div id="mobile-menu" class="mobile__menu" aria-hidden="true">
+					<button class="mobile__menu-close" tabindex="-1"><span
+							tabindex="-1"
+							class="screen-reader-text"
+						><?php echo esc_html__( 'Close Menu', 'christileeson' ); ?></span></button>					
+					<nav
+						class="=mobile__main-navigation"
+						aria-label="<?php echo esc_attr__( 'Mobile main navigation', 'christileeson' ); ?>"
+					><?php
+						wp_nav_menu(
+							[
+								'theme_location' => 'main-navigation',
+								'menu_id'        => 'main-navigation',
+								'container'      => false,
+								'echo'           => true,
+								'depth'          => 1
+							]
+						);
+						?>
+					</nav>
+				</div>
 			</div>
 		</div>
 	</header>
