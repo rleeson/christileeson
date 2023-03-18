0eff94fbad73aa6df6153205ec3fc2794893779a
Ryan Leeson <sysadmin@ryanleeson.com>
2023-03-18 11:24:55 -0400

    - Add site footer

--- wp-content/themes/custom/christileeson/footer.php
+++ wp-content/themes/custom/christileeson/footer.php
@@ -10,8 +10,16 @@
  */
 ?>
 	</div>
-	<footer class="site-footer">
-		
+	<footer class="footer">
+		<p class="footer__attribution">
+			<span class="footer__attribution-name"><?php 
+				echo esc_html( 
+					sprintf( "&copy;%s Christi Leeson", gmdate("Y") ), 
+					"christileeson" 
+				); ?></span>&nbsp;|&nbsp;
+			<span class="footer__attribution-legal"><?php 
+				echo esc_html( "All rights reserved", "christileeson" ); ?></span>
+		</p>
 	</footer>
 	<?php wp_footer(); ?>
 </body>
