e08919b4f529dfe38d46e787d18747b8db9baf86
Ryan Leeson <ryan.leeson@kanopistudios.com>
2023-11-19 16:25:44 -0500

    - Mobile menu - increase text link size - Allow featured images on all posts - Adjust Cover block min-height to auto

--- wp-content/themes/custom/christileeson/functions.php
+++ wp-content/themes/custom/christileeson/functions.php
@@ -86,7 +86,7 @@ add_action(
 		/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*/
-		add_theme_support( 'post-thumbnails', [ 'page' ] );
+		add_theme_support( 'post-thumbnails' );
 
 		/**
 		 * Register theme menus
